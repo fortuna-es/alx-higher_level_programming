9cd7a3d772f49bc0fd390b24ebaaa9becc034c25
fortuna-es <56885624+fortuna-es@users.noreply.github.com>
2022-09-06 22:45:05 +0300

    0x12-js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // Handle arguments with Javascript
 
-if (process.argv.length < 3) {
+if (process.argv.length < 1) {
   console.log('No argument');
-} else if (process.argv.length === 3) {
+} else if (process.argv.length === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
